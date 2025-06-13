bbc308c8fb6bfeae8ddea7306974f5f40534f4a7
Shriya Singh <siu@Shriyas-MacBook-Pro.local>
2025-06-13 17:32:07 +0530

    img fixes

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import App from './App.vue'
 import { setUserInfo } from './data/mocks/getUserInfo'
 import { setCreditInfo } from './data/mocks/getCreditInfo'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-}
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/service-worker.js')
+// }
 
 setUserInfo()
 setCreditInfo()
